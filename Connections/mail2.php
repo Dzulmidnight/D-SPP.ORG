178f697338150c9aec03322ccc05d5aacc041be2
Dzulmidnight <yasser.midnight@gmail.com>
2017-01-23 13:12:49 -0600

    modificación mail

--- Connections/mail2.php
+++ Connections/mail2.php
@@ -2,24 +2,24 @@
 include_once("../PHPMailer/class.phpmailer.php");
 include_once("../PHPMailer/class.smtp.php");
 
-$mail = new PHPMailer();
+/*$mail = new PHPMailer();
 $mail->IsSMTP();
 //$mail->SMTPSecure = "ssl";
 $mail->Host = "mailtrap.io";
 //$mail->Port = 25;
-$mail->Port = 2525;
+$mail->Port = 25;
 $mail->SMTPAuth = true;
-$mail->Username = "d4071ef0fb933e";
-$mail->Password = "8e53290c3bd811";
+$mail->Username = "99f9a8c6200b52";
+$mail->Password = "943bba851a952d";
 //$mail->SMTPDebug = 1;
 
 $mail->From = "soporte@d-spp.org";
 $mail->FromName = utf8_decode("CERTIFICACIÓN-CERTIFICATION SPP");
-
+$mail->AddBCC("yasser.midnight@gmail.com", "correo Oculto");
 
 $correoCert = "cert@spp.coop";
 
-/*
+*/
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
@@ -34,8 +34,8 @@ $mail->SMTPDebug = 1;
 
 $mail->From = "soporte@d-spp.org";
 $mail->FromName = utf8_decode("CERTIFICACIÓN-CERTIFICATION SPP");
-$mail->AddBCC("yasser.midnight@gmail.com", "correo Oculto");
+$mail->AddBCC("yasser.midnight@gmail.com");
 
 $correoCert = "cert@spp.coop";
-*/
+
  ?>
