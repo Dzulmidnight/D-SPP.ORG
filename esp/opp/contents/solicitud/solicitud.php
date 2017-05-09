a76aaf7d7e54ee2a5cf6c5a7ca882fe71a74d5f1
Dzulmidnight <yasser.midnight@gmail.com>
2017-05-08 22:07:33 -0500

    modificación 08_05_2017(2)

--- esp/opp/contents/solicitud/solicitud.php
+++ esp/opp/contents/solicitud/solicitud.php
@@ -1,6 +1,64 @@
 <?php 
+require_once('../Connections/dspp.php'); 
+require_once('../Connections/mail.php');
+
+mysql_select_db($database_dspp, $dspp);
+
+if (!isset($_SESSION)) {
+  session_start();
+	
+	$redireccion = "../index.php?OPP";
+
+	if(!$_SESSION["autentificado"]){
+		header("Location:".$redireccion);
+	}
+}
+
+if (!function_exists("GetSQLValueString")) {
+	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+	{
+	  if (PHP_VERSION < 6) {
+	    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+	  }
+
+	  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+	  switch ($theType) {
+	    case "text":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;    
+	    case "long":
+	    case "int":
+	      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+	      break;
+	    case "double":
+	      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+	      break;
+	    case "date":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;
+	    case "defined":
+	      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+	      break;
+	  }
+	  return $theValue;
+	}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+$idopp = $_SESSION['idopp'];
 $ano_actual = date('Y', time());
 echo $ano_actual;
+
+//consultamos las solicitudes que tiene en el año
+$row_solicitudes = mysql_query("SELECT idsolicitud_certificacion FROM solicitud_certificacion WHERE idopp = $idopp AND FROM_UNIXTIME(fecha_registro,'%Y') = $ano_actual", $dspp) or die(mysql_error());
+$total = mysql_num_rows($row_solicitudes);
+
+echo "EL TOTAL ES: ".$total;
  ?>
 
 <ul class="nav nav-pills">
