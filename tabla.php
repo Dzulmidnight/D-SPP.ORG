b3e4f0eb4eff8c793d2b0433cc65fa6228651e84
Dzulmidnight <yasser.midnight@gmail.com>
2017-06-09 21:59:24 -0500

    mensajes renovacion(empresas)

---
+++ tabla.php
@@ -0,0 +1,56 @@
+                <table style="font-family: Tahoma, Geneva, sans-serif; font-size: 13px; color: #797979;" border="0" width="650px">
+                  <tbody>
+                    <tr>
+                      <th rowspan="1" scope="col" align="center" valign="middle" width="170"><img src="http://d-spp.org/img/mailFUNDEPPO.jpg" alt="Simbolo de Pequeños Productores." width="120" height="120" /></th>
+                      <th scope="col" align="left" width="500"><strong><h3>Aviso de Renovación de Certificado SPP</h3></strong></th>
+                    </tr>
+                    <tr>
+                      <td style="text-align:justify; padding-top:2em" colspan="2">
+                     
+                        <p>Estimados Representantes de <strong style="color:red">'.$nombre_empresa.', (<u>'.$abreviacion_empresa.'</u>)</strong>:</p>
+                        
+                        <p>Por este conducto se les informa la necesidad de renovación de su Registro SPP. La fecha de su vigencia de su Registro SPP es <strong style="color:red">'.$fecha_vigencia.'</strong>, por lo que deben proceder con la evaluación anual.</p>
+                        
+                        <p>De acuerdo a los procedimientos del SPP, se puede llevar a cabo la evaluación dos meses antes de la fecha de vigencia o máximo dos meses después.  Si la evaluación se realiza dos meses después, se esperaría que el dictamen se obtuviera 4 meses después  (de la fecha de vencimiento del Registro) como plazo máximo, para obtener el dictamen positivo de parte del Organismo de Certificación.</p>
+                      
+                        <p>Queremos enfatizar que actualmente existen políticas para la suspensión y/o cancelación del Registro por lo que si ustedes no solicitan a tiempo pueden ser acreedores de una suspensión.</p>
+                        
+                        <p>Agradeciendo su atención, nos despedimos y enviamos saludos del SPP GLOBAL.</p>
+
+                        <p style="color:#2c3e50"><b>En caso de haber iniciado ya su proceso de renovación del registro por favor hacer caso omiso a este mensaje</b></p>
+                        
+                        <p>CUALQUIER INCONVENIENTE FAVOR DE NOTIFICARLO A SPP GLOBAL AL CORREO <strong>cert@spp.coop</strong></p>
+                      
+                      </td>
+                    </tr>
+                    <tr>
+                      <th colspan="2" style="padding-top:5em;">
+                        <b>English below</b>
+                        <hr>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th scope="col" align="left" width="500"><strong><h3>SPP Registration Renewal Notice</h3></strong></th>
+                    </tr>
+                    <tr>
+                      <td style="text-align:justify; padding-top:2em" colspan="2">
+                     
+                        <p>Dear <strong style="color:red">'.$nombre_empresa.', (<u>'.$abreviacion_empresa.'</u>)</strong> Representatives</p>
+                        
+                        <p>You are hereby informed of the need for renewal of your SPP Registration. The effective date of your SPP Registration is: <strong style="color:red">'.$fecha_vigencia.'</strong>, so you must proceed with the annual evaluation.</p>
+                        
+                        <p>According to the procedures of the SPP, the evaluation can be carried out two months before the effective date of your Registration or maximum two months later. If the evaluation is carried out two months later, it would be expected that the opinion would be obtained 4 months <span style="color:red">later (from the expiration date of the Registration) as a maximum term</span>, in order to obtain a positive opinion from the Certification Entity.</p>
+                      
+                        <p>We want to emphasize that there are currently policies for the suspension and / or cancellation of the Registration, so if you do not apply on time you may be entitled to a suspension.</p>
+                        
+                        <p>Thank you for your attention, we said goodbye and we send greetings from SPP GLOBAL.</p>
+
+                        <p style="color:#2c3e50"><b>If you have already started your registration renewal process please ignore this message</b></p>
+                        
+                        <p>ANY INCONVENIENT PLEASE NOTICE TO SPP GLOBAL TO THE MAIL <strong>cert@spp.coop</strong></p>
+                      
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
